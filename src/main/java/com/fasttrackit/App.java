1397254a7b7ba6c26e13a1d60818abbe6695a91e
peter <peter.j.krisztian@gmail.com>
2019-08-28 02:23:49 +0300

    project

--- src/main/java/com/fasttrackit/App.java
+++ src/main/java/com/fasttrackit/App.java
@@ -1,13 +1,38 @@
 package com.fasttrackit;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+import java.time.LocalDate;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+        Animal dog1 = new Animal();
+        dog1.name = "Rex";
+        dog1.age = 2;
+        dog1.favoriteFood = "Pedigri";
+        dog1.favoriteRecreationActivity = "Running";
+        dog1.healthLevel = 9;
+        dog1.hungryLevel = 1;
+        dog1.spiritLevel = 7;
+
+        Food dogFood1 = new Food();
+        dogFood1.name = "Pedigri";
+        dogFood1.amount = 500;
+        dogFood1.availability = true;
+        dogFood1.price = 25.50;
+        dogFood1.expirationDate = 25;
+
+        RecreationActivity running = new RecreationActivity();
+        running.name = "Running";
+
+        Adopter adopter1 = new Adopter();
+        adopter1.availableMoney = 500;
+        adopter1.name = "David";
+
+
+
+        ;
     }
+
+
 }
